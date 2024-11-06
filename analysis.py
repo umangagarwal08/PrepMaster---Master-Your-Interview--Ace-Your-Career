14ddb48833a99ad27c85690ea0764f861f379159
Umang Agarwal <59832370+umangagarwal08@users.noreply.github.com>
2024-11-06 22:04:50 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -10,16 +10,18 @@ genai.configure(api_key='AIzaSyC8550xiHFPK3IVdYSL80HMiDUp6y16zc0')
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 def model_gen(topic):
-    difficulty_based="Generate interview questions of varying difficulty levels based on the"+ topic+". Include:\
-                        - Technical questions for coding or concepts.\
-                        - Situational questions that assess problem-solving in real-world scenarios.\
-                        - Behavioral questions to gauge soft skills.\
-                          Divide the questions by difficulty: easy, medium, and hard.\
-                        For each interview question generated, provide a hint \
-                                 or explanation that can help the student understand how to \
-                                 approach or answer the question. These hints should give a \
-                                 concise explanation of key concepts or provide steps for \
-                                 solving the question Generate atleat 10 question of each type"
+    difficulty_based="Generate a set of tricky coding interview questions focused on the topic: "+topic+""" Organize 
+      the questions into three difficulty levels — easy, medium, and hard —
+      with at least 10 questions in each category.
+      Easy: Create questions that test basic understanding of the topic, 
+      including fundamental concepts, simple problem-solving, and standard coding challenges.
+      Medium: Develop questions that require deeper problem-solving skills and intermediate
+        knowledge of the topic, including algorithm optimization, handling edge cases, and applying theoretical knowledge in code.
+      Hard: Formulate challenging questions that demand advanced problem-solving abilities and 
+      comprehensive understanding of the topic, such as complex algorithms, data structure manipulation, and optimized solutions for real-world scenarios.
+      For each question, provide a hint or explanation that will help the candidate approach the 
+      problem or understand the concepts required to solve it. Hints should be concise, highlighting 
+      key ideas, useful steps, or strategies for problem-solving."""
     difficulty_ans=model.generate_content(difficulty_based)
 
     category_based="Generate three types of interview questions for"+ topic+":\
