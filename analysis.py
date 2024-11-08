b29e7cad03edcd712505005d7ad2b4bdb9ce711f
Umang Agarwal <59832370+umangagarwal08@users.noreply.github.com>
2024-11-08 23:54:24 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,14 +3,14 @@ import google.generativeai as genai # type: ignore
 
 import os
 
-# Configure the API key
-# api_key = os.getenv("GOOGLE_API_KEY")  # Recommended approach
-genai.configure(api_key='AIzaSyC8550xiHFPK3IVdYSL80HMiDUp6y16zc0')
+#Configure the API key
+api_key = os.getenv("GOOGLE-API-KEY")  # Recommended approach
+genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash") 
 
-def model_gen(topic):
-    difficulty_based="Generate a set of tricky coding interview questions in which we have to code focused on the topic: "+topic+""" Organize 
+def coding_ques(topic,company):
+    coding_based=f"Generate a set of tricky coding interview questions for {company} company in which we have to code focused on the topic: "+topic+""" Organize 
       the questions into three difficulty levels — easy, medium, and hard —
       with at least 10 questions in each category.
       Easy: Create questions that test basic understanding of the topic, 
@@ -22,9 +22,12 @@ def model_gen(topic):
       For each question,provide a hint or explanation in next line that will help the candidate approach the 
       problem or understand the concepts required to solve it. Hints should be concise, highlighting 
       key ideas, useful steps, or strategies for problem-solving."""
-    difficulty_ans=model.generate_content(difficulty_based)
+    coding_ans=model.generate_content(coding_based)
+    return{st.write(coding_ans.text)}
 
-    category_based="Generate three types of interview questions for"+ topic+":\
+
+def theory_ques(topic,company):
+    theory_based="Generate three types of easy to  medium interview questions for  {company} company on "+ topic+":\
                     - **Conceptual** questions to assess basic knowledge of the topic.\
                     - **Applied** questions that require practical application of the topic.\
                     - **Scenario-based** questions, where the candidate has to apply knowledge in a real-world scenario.\
@@ -34,12 +37,12 @@ def model_gen(topic):
                                  concise explanation of key concepts or provide steps for \
                                  solving the question Generate atleat 10 question of each type give the hint in next line"
     
-    category_ans=model.generate_content(category_based)
+    theory_ans=model.generate_content(theory_based)
 
-    
-    return{#st.subheader(body = "Difficulty Based Questions"),
-           st.write(difficulty_ans.text),
-           #st.subheader(body = "Category Based Questions"),
-           st.write(category_ans.text)
-    } 
+    return{st.write(theory_ans.text)} 
+
+
+
+
+
 
